69951df005edf019846da231799c0810d79a376f
John Coote <john.coote@sherpa.net.au>
2023-11-04 14:24:11 +1100

    add guard to is_image method

--- app/models/document.rb
+++ app/models/document.rb
@@ -10,6 +10,8 @@ class Document < ApplicationRecord
   end
 
   def is_image?
+    return false unless self.link.present?
+
     self.link.ends_with?('.jpg', '.jpeg', '.png', '.gif', '.avif')
   end
 end
